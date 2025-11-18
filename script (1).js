56365622a9ceee11cfe4217ef662d76c20a10dc4
abije0 <abitad558@gmail.com>
2025-11-19 02:56:56 +0300

    Add files via upload

---
+++ script (1).js
@@ -0,0 +1,7 @@
+//simple javascript interaction
+//this shows a pop-up message when the page loads
+window.addEventListener("load",function(){
+    console.log("website loaded successfully!");
+
+//cute greeting message for visitors alert("welcome to my agriculture portfolio!");
+});
